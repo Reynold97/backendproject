1087bc03ef2ea40f94a2f4257c978238825230c0
Reynold97 <reynoldalejandro46@gmail.com>
2023-06-13 10:22:52 -0400

    added relationship to the Post fetch

--- app/schemas.py
+++ app/schemas.py
@@ -2,6 +2,19 @@ from pydantic import BaseModel, EmailStr
 from datetime import datetime
 from typing import Optional
 
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str 
+
+
+class UserOut(BaseModel):
+    id: int
+    email: EmailStr
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
+
 class PostBase(BaseModel):
     title: str
     content: str 
@@ -16,20 +29,7 @@ class PostOut(PostBase):
     id: int    
     created_at: datetime
     owner_id: int
-
-    class Config:
-        orm_mode = True
-
-
-class UserCreate(BaseModel):
-    email: EmailStr
-    password: str 
-
-
-class UserOut(BaseModel):
-    id: int
-    email: EmailStr
-    created_at: datetime
+    owner: UserOut
 
     class Config:
         orm_mode = True
