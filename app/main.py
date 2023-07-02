7126675fbd25bacc74a7ba01fbe47ce1ec5ce0c0
Reynold97 <reynoldalejandro46@gmail.com>
2023-07-02 12:57:53 -0400

    Added CORS policy

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app import models
 from app.database import engine 
@@ -9,7 +10,17 @@ from app.config import settings
 #models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-    
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
