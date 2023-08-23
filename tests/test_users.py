82d478943f656af85dd54160d2afdf9dba5f4101
Reynold97 <reynoldalejandro46@gmail.com>
2023-08-23 09:55:45 -0400

    1st API test (user table)

---
+++ tests/test_users.py
@@ -0,0 +1,36 @@
+import pytest
+
+from app import schemas
+
+from fastapi.testclient import TestClient
+import pytest
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from app.main import app
+
+from app.config import settings
+from app.database import get_db
+from app.database import Base
+from alembic import command
+
+
+engine = create_engine(f"postgresql://{settings.DATABASE_USERNAME}:{settings.DATABASE_PASSWORD}@{settings.DATABASE_HOSTNAME}:{settings.DATABASE_PORT}/{settings.DATABASE_NAME}_test")
+
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+@pytest.fixture()
+def client():
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
+    yield TestClient(app)
+
+
+def test_create_user(client):
+    res = client.post(
+        "/users/", json={"email": "reynold@gmail.com", "password": "password123"})
+
+    new_user = schemas.UserOut(**res.json())
+    assert new_user.email == "reynold@gmail.com"
+    assert res.status_code == 201
